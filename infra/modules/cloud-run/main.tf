597b18ef389d6ba8385d52ce4f33b2f99f949339
Markus Schmidberger <markus.schmidberger@gmail.com>
2026-04-02 20:16:55 +0100

    fix: add env vars to Cloud Run lifecycle ignore_changes
    Prevents Terraform from stripping Cloud Build-managed env vars
    (CORS_ALLOWED_ORIGINS, GOOGLE_CLOUD_PROJECT) on apply.

--- infra/modules/cloud-run/main.tf
+++ infra/modules/cloud-run/main.tf
@@ -28,6 +28,7 @@ resource "google_cloud_run_v2_service" "backend" {
   lifecycle {
     ignore_changes = [
       template[0].containers[0].image,
+      template[0].containers[0].env,
       client,
       client_version,
       traffic,
@@ -56,6 +57,7 @@ resource "google_cloud_run_v2_service" "frontend" {
   lifecycle {
     ignore_changes = [
       template[0].containers[0].image,
+      template[0].containers[0].env,
       client,
       client_version,
       traffic,
