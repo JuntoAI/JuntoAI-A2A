ac646cf7de631966303268034527b5d0703f1a15
Markus Schmidberger <markus.schmidberger@gmail.com>
2026-04-07 15:14:10 +0100

    infra: add Firestore composite index for negotiation_sessions (owner_email + created_at)

--- infra/modules/firestore/main.tf
+++ infra/modules/firestore/main.tf
@@ -13,3 +13,21 @@ resource "google_firestore_database" "database" {
 
   depends_on = [google_project_service.firestore]
 }
+
+# Composite index for negotiation history query:
+# WHERE owner_email == X AND created_at >= Y ORDER BY created_at DESC
+resource "google_firestore_index" "negotiation_sessions_owner_created" {
+  project    = var.gcp_project_id
+  database   = google_firestore_database.database.name
+  collection = "negotiation_sessions"
+
+  fields {
+    field_path = "owner_email"
+    order      = "ASCENDING"
+  }
+
+  fields {
+    field_path = "created_at"
+    order      = "DESCENDING"
+  }
+}
